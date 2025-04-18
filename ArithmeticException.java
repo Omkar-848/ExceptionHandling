b6dd815a01b70ce4cab2d6e97bb669b4426a9b8d
OMKAR CHAVAN <ovchavan3456@gmail.com>
2025-04-18 10:00:18 -0700

    Add files via upload

---
+++ ArithmeticException.java
@@ -0,0 +1,14 @@
+package ExceptionHandling;
+
+public class ArithmeticException {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 8;
+
+        System.out.println(a + b); // Addition
+        System.out.println(a / b); // Division
+        System.out.println(a * b); // Multiplication
+        System.out.println(a - b); // Subtraction
+    }
+    
+}
